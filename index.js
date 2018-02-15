0e252cd6192535e2ff5cae5f5dadcedf624bde0d
ilhomelv <ilhomelv@gmail.com>
2018-02-15 13:10:41 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+//remember any change made on here may not work on page
+//until you restart the node js server from terminal:   node index
+
+
+var express = require('express');
+var socket = require('socket.io');  //server socket
+
+//App  setup
+var app = express();
+var server = app.listen(4000, function(){
+  console.log('listening port 4000');
+});
+
+//Static files
+app.use(express.static('public'));
+
+//Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket){
+  console.log('made socket connection...', socket.id);
+
+//receive data from front User socket and send it to all users connected on Server socket
+  socket.on('chat', function(data){
+    io.sockets.emit('chat', data);
+  });
+
+  socket.on('typingMsg', function(data){
+    socket.broadcast.emit('typingMsg', data);
+  });
+});
---
+++ public/chat.js
@@ -0,0 +1,33 @@
+//make connection
+//var socket is not same variable as one in index.js
+var socket = io.connect('http://localhost:4000'); //user socket
+
+var message = document.getElementById('message'),
+    handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output'),
+    typing = document.getElementById('typing');
+
+
+//emit events
+btn.addEventListener('click', function(){
+  socket.emit('chat', {
+    message: message.value,
+    handle: handle.value
+  });
+});
+
+message.addEventListener('keypress', function(){
+  socket.emit('typingMsg', handle.value);
+})
+
+
+//listen events
+socket.on('chat', function(data){
+    typing.innerHTML = ""; //clears: is typing a message...
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typingMsg', function(data){
+  typing.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
